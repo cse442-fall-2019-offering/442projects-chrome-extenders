0e56a965adb0b2cce35d9c219b3939d887fb3072
Sarra Souissi <sarrasouissi@Sarras-MacBook-Pro-4.local>
2019-09-26 16:38:34 -0400

    setup for get request unit tests

--- dictext/unit-tests/api-tests.py
+++ dictext/unit-tests/api-tests.py
@@ -12,6 +12,7 @@ def get_definitions_for_imput_word_in_terminal():
 
     word = input("Enter word you'd like to get the defintion of: ")
     print(word)
+    print('\n')
 
     app_id = d_app_id
     app_key = d_app_key
@@ -33,9 +34,28 @@ def get_definitions_for_imput_word_in_terminal():
 
     senses_array = data['results'][0]['lexicalEntries'][0]['entries'][0]['senses']
 
+    definitions_array = []
     for i in senses_array:
-        print(i['definitions'][0])
-        print('\n')
+
+        definitions_array.append(i['definitions'][0])
+        # print(i['definitions'][0])
+        # print('\n')
+
+        print(definitions_array)
+
+    return definitions_array
+
+
+def get_synonyms():
+    return null
+
+
+def get_antonyms():
+    return null
+
+
+def get_example():
+    return null
 
 
 if __name__ == "__main__":
