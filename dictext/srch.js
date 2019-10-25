6a50b14e4052e6469a7d6e574be701102dd7ea3b
Brett Baker <brettbaker@Bretts-MacBook-Air.local>
2019-10-25 12:36:47 -0400

    deleted unnecessary files, made extension display all known definitions instead of just one, added an error message when a word is not found

--- dictext/srch.js
+++ dictext/srch.js
@@ -21,24 +21,36 @@ document.addEventListener("DOMContentLoaded", function() {
   const QUERY_BOX = document.getElementById("query");
   const DEF_LIST = document.getElementById("definitions");
   const SYN_LIST = document.getElementById("synonyms");
+  const ERROR = document.getElementById("errorBox");
 
   function queried(e) {
     e.preventDefault();
     DEF_LIST.innerHTML = "";
     SYN_LIST.innerHTML = "";
+    ERROR.innerHTML = "";
+
     let queryValue = document.getElementsByTagName("input")[0].value;
-
-    let entry = document.createElement("li");
-    console.log("1");
     let response = get_request(queryValue);
-    console.log(response.defintions[0]);
-    entry.appendChild(document.createTextNode(response.defintions[0]));
-    DEF_LIST.appendChild(entry);
 
-    entry = document.createElement("li");
-    console.log("2");
-    entry.appendChild(document.createTextNode(response.synonyms[0]));
-    SYN_LIST.appendChild(entry);
+    if (!Object.keys(response).length) {
+        let entry = document.createElement("h3");
+        entry.appendChild(document.createTextNode("Word not found"));
+        ERROR.appendChild(entry);
+    }
+    else {
+        let entry = document.createElement("li");
+        
+        for (i=0;i<response.defintions.length;i++) {
+            entry = document.createElement("li");
+            entry.appendChild(document.createTextNode(response.defintions[i]));
+            DEF_LIST.appendChild(entry);
+        }
+        for (i=0;i<response.synonyms.length;i++) {
+            entry = document.createElement("li");
+            entry.appendChild(document.createTextNode(response.synonyms[i]));
+            SYN_LIST.appendChild(entry);
+        }
+    }
   }
 
   QUERY_BOX.addEventListener("submit", queried);
